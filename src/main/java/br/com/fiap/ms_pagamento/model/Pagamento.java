5704dec5f38138ddced8a6df8e81f39972e3a6bd
felipe.adrilima@gmail.com <labsfiap@fiap.com.br>
2024-08-07 09:41:16 -0300

    FL-Alteração na class Pagamento-criação da tabela tb_pagamento e a definição de algumas variáveis

--- src/main/java/br/com/fiap/ms_pagamento/model/Pagamento.java
+++ src/main/java/br/com/fiap/ms_pagamento/model/Pagamento.java
@@ -1,5 +1,6 @@
 package br.com.fiap.ms_pagamento.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,16 +13,30 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 
+@Entity
+@Table(name = "tb_pagamento")
 public class Pagamento {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)       //Não pode ser nulo
     private BigDecimal valor;
+
     private String nome;            //nome do cartão
     private String numeroCartao;    //número do cartão
     private String validade;        //validade do cartão - MM/AA
     private String codigoSeguranca; //código de segurança do cartão - XXX
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
     private Status status;          //Status do pagamento
+
+    @Column(nullable = false)
     private Long pedidoId;          //Id do pedido
+
+    @Column(nullable = false)
     private Long formaPagamento;    //1 - dinheiro | 2 - cartão | 3 - pix
 
 }

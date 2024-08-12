11713ed36fdbcea1f83c1d1c9bf56d1c35f58e46
Felipe LIma <felipe.adrilima@gmail.com>
2024-08-12 10:52:13 -0300

    FL-Criação da controller de pagamento com o método findAll

---
+++ src/main/java/br/com/fiap/ms_pagamento/controller/PagamentoController.java
@@ -0,0 +1,26 @@
+package br.com.fiap.ms_pagamento.controller;
+
+import br.com.fiap.ms_pagamento.dto.PagamentoDTO;
+import br.com.fiap.ms_pagamento.model.Pagamento;
+import br.com.fiap.ms_pagamento.service.PagamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/pagamentos")
+public class PagamentoController {
+
+    @Autowired
+    private PagamentoService service;
+
+    @GetMapping
+    public ResponseEntity<List<PagamentoDTO>> findAll() {
+        List<PagamentoDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+}

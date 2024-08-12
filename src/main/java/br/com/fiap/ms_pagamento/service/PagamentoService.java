77030118559d7bb4f657d9a42854e1967ffc913b
Felipe LIma <felipe.adrilima@gmail.com>
2024-08-12 10:51:46 -0300

    FL-Criação da service de pagamento com o método findAll

---
+++ src/main/java/br/com/fiap/ms_pagamento/service/PagamentoService.java
@@ -0,0 +1,25 @@
+package br.com.fiap.ms_pagamento.service;
+
+import br.com.fiap.ms_pagamento.dto.PagamentoDTO;
+import br.com.fiap.ms_pagamento.model.Pagamento;
+import br.com.fiap.ms_pagamento.repository.PagamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PagamentoService {
+
+    @Autowired
+    private PagamentoRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<PagamentoDTO> findAll() {
+        List<Pagamento> list = repository.findAll();
+        return list.stream().map(PagamentoDTO::new).collect(Collectors.toList());
+    }
+
+}

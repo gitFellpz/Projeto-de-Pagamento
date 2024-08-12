3ab99096962ae658139cb425b2d3b4de883cfb97
Felipe LIma <felipe.adrilima@gmail.com>
2024-08-12 11:22:06 -0300

    FL-Alterando pagamento controller - Criando método insert

--- src/main/java/br/com/fiap/ms_pagamento/controller/PagamentoController.java
+++ src/main/java/br/com/fiap/ms_pagamento/controller/PagamentoController.java
@@ -3,13 +3,13 @@ package br.com.fiap.ms_pagamento.controller;
 import br.com.fiap.ms_pagamento.dto.PagamentoDTO;
 import br.com.fiap.ms_pagamento.model.Pagamento;
 import br.com.fiap.ms_pagamento.service.PagamentoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,5 +29,16 @@ public class PagamentoController {
     public ResponseEntity<PagamentoDTO> findById (@PathVariable Long id) {
         PagamentoDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PagamentoDTO> insert (@RequestBody @Valid PagamentoDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
     }
 }

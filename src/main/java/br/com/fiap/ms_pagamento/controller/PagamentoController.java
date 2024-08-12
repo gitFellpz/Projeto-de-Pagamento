f7966ad76ccb4904141f53a1a9814b9adba2266a
Felipe LIma <felipe.adrilima@gmail.com>
2024-08-12 11:09:57 -0300

    FL-Alterando pagamento controller - Adicionando método findById

--- src/main/java/br/com/fiap/ms_pagamento/controller/PagamentoController.java
+++ src/main/java/br/com/fiap/ms_pagamento/controller/PagamentoController.java
@@ -6,6 +6,7 @@ import br.com.fiap.ms_pagamento.service.PagamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class PagamentoController {
     @GetMapping
     public ResponseEntity<List<PagamentoDTO>> findAll() {
         List<PagamentoDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PagamentoDTO> findById (@PathVariable Long id) {
+        PagamentoDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
     }
 }
